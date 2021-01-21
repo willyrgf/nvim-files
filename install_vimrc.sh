886ba006a1b91ce865e7b8dd54a1f0b56d9720c9
willyrgf <willyr.goncalves@gmail.com>
2021-01-21 19:36:53 -0300

    move all nvim files to a dir

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -23,7 +23,7 @@ _install() {
     fi
 
     # copy vimrc's
-    if ! cp -av *.{vim,json} ~/; then
+    if ! cp -av configs/*.{vim,json} ~/; then
         echo "error on copy the vimrcs to home"
         return 1
     fi
